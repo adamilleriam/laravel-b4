0721858c14638ee59a99b1f3530f91727f577288
Trainer PNT <trainer@piit.local>
2019-09-12 21:10:07 +0600

    Post list shown

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $data['posts'] = DB::table('posts')->get();
+        return view('post/index',$data);
+    }
     public function create()
     {
         return view('post/create');
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('posts','PostController@index');
 Route::get('post/create','PostController@create');
 Route::post('post/store','PostController@store');
